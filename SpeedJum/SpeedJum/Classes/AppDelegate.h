98ba1e9ebd52924025f58a41b16f5cc82db44f8d
Ali Ashoor <ali.ash.kh@gmail.com>
2014-03-18 00:10:05 +0300

    iPhone_Not_Mac

---
+++ SpeedJum/SpeedJum/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  SpeedJum
+//
+//  Created by Ali on 3/18/14.
+//  Copyright Ali Ashoor 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ SpeedJum/SpeedJum/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SpeedJum
+//
+//  Created by Ali on 3/18/14.
+//  Copyright Ali Ashoor 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
