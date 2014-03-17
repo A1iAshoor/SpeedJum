242c89f6f3ebf5f763c492f9b4d2cc65fcc6aefa
Ali Ashoor <ali.ash.kh@gmail.com>
2014-03-17 23:58:18 +0300

    first

---
+++ SpeedJum/SpeedJum/Classes/HelloWorldLayer.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorldLayer.h
+//  SpeedJum
+//
+//  Created by Ali on 3/17/14.
+//  Copyright Ali Ashoor 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCNode
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
