ad82e5bfa61a4d6f24e171508c342e18c0cc2116
Ali Ashoor <ali.ash.kh@gmail.com>
2014-03-27 01:24:16 +0300

    2014_03_27_first_implementation

--- SpeedJum.spritebuilder/Source/AppDelegate.m
+++ SpeedJum.spritebuilder/Source/AppDelegate.m
@@ -23,8 +23,6 @@
  * THE SOFTWARE.
  */
 
-#import "cocos2d.h"
-
 #import "AppDelegate.h"
 #import "CCBuilderReader.h"
 
@@ -33,18 +31,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Configure Cocos2d with the options set in SpriteBuilder
-    NSString* configPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Published-iOS"]; // TODO: add support for Published-Android support
+    NSString* configPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Published-iOS"];
     configPath = [configPath stringByAppendingPathComponent:@"configCocos2d.plist"];
     
     NSMutableDictionary* cocos2dSetup = [NSMutableDictionary dictionaryWithContentsOfFile:configPath];
     
-    // Note: this needs to happen before configureCCFileUtils is called, because we need apportable to correctly setup the screen scale factor.
-#ifdef APPORTABLE
+    #ifdef APPORTABLE
     if([cocos2dSetup[CCSetupScreenMode] isEqual:CCScreenModeFixed])
         [UIScreen mainScreen].currentMode = [UIScreenMode emulatedMode:UIScreenAspectFitEmulationMode];
     else
         [UIScreen mainScreen].currentMode = [UIScreenMode emulatedMode:UIScreenScaledAspectFitEmulationMode];
-#endif
+    #endif
     
     // Configure CCFileUtils to work with SpriteBuilder
     [CCBReader configureCCFileUtils];
--- SpeedJum.spritebuilder/Source/MainScene.m
+++ SpeedJum.spritebuilder/Source/MainScene.m
@@ -8,6 +8,95 @@
 
 #import "MainScene.h"
 
-@implementation MainScene
+@implementation MainScene {
+    CCSprite *_ball;
+    CCPhysicsNode *_physicsNode;
+    
+    NSTimeInterval _sinceTouch;
+    
+    CGFloat _scrollSpeed;
+    
+    CCButton *_restartButton;
+    
+    BOOL _gameOver;
+    
+    NSInteger _points;
+    CCLabelTTF *_scoreLabel;
+}
+
+- (void)didLoadFromCCB {
+//    _scrollSpeed = 80.f;
+    self.userInteractionEnabled = TRUE;
+
+    // set this class as delegate
+    _physicsNode.collisionDelegate = self;
+}
+
+#pragma mark - Touch Handling
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [_ball.physicsBody applyImpulse:ccp(0, 400.f)];
+    [_ball.physicsBody applyAngularImpulse:999.f];
+    _sinceTouch = 0.f;
+}
+
+#pragma mark - CCPhysicsCollisionDelegate
+
+//-(BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair hero:(CCNode *)hero level:(CCNode *)level {
+//    [self gameOver];
+//    return true;
+//}
+
+//-(BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair hero:(CCNode *)hero goal:(CCNode *)goal {
+//    [goal removeFromParent];
+//    
+//    return TRUE;
+//}
+
+#pragma mark - Game Actions
+
+- (void)gameOver {
+    if (!_gameOver) {
+        _scrollSpeed = 0.f;
+        _gameOver = TRUE;
+        _restartButton.visible = TRUE;
+        
+        _ball.rotation = 90.f;
+        _ball.physicsBody.allowsRotation = FALSE;
+        [_ball stopAllActions];
+        
+        CCActionMoveBy *moveBy = [CCActionMoveBy actionWithDuration:0.2f position:ccp(-2, 2)];
+        CCActionInterval *reverseMovement = [moveBy reverse];
+        CCActionSequence *shakeSequence = [CCActionSequence actionWithArray:@[moveBy, reverseMovement]];
+        CCActionEaseBounce *bounce = [CCActionEaseBounce actionWithAction:shakeSequence];
+        
+        [self runAction:bounce];
+    }
+}
+
+#pragma mark - Update
+
+- (void)update:(CCTime)delta {
+    // clamp velocity
+    float yVelocity = clampf(_ball.physicsBody.velocity.y, -1 * MAXFLOAT, 200.f);
+    _ball.physicsBody.velocity = ccp(0, yVelocity);
+    _ball.position = ccp(_ball.position.x + delta * _scrollSpeed, _ball.position.y);
+    
+    _sinceTouch += delta;
+    
+    _ball.rotation = clampf(_ball.rotation, -30.f, 90.f);
+    
+    if (_ball.physicsBody.allowsRotation) {
+        float angularVelocity = clampf(_ball.physicsBody.angularVelocity, -2.f, 1.f);
+        _ball.physicsBody.angularVelocity = angularVelocity;
+    }
+    
+    if ((_sinceTouch > 0.5f)) {
+        [_ball.physicsBody applyAngularImpulse:-40000.f*delta];
+    }
+    
+    _physicsNode.position = ccp(_physicsNode.position.x - (_scrollSpeed *delta), _physicsNode.position.y);
+
+}
 
 @end
